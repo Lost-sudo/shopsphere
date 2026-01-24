b9bb866a6971267911c0f3011915516608029460
John Patrick Paraon <johnpatrickparaon.dev@gmail.com>
2026-01-24 12:45:56 +0800

    update: updated the container.ts to include the verification service as dependency to auth service

--- server/src/config/container.ts
+++ server/src/config/container.ts
@@ -2,8 +2,14 @@ import { AuthController } from "../controllers/auth.controller";
 import { UserRepository } from "../repositories/user.repository";
 import { AuthService } from "../services/auth.service";
 import { RefreshSessionService } from "../services/refreshSession.service";
+import { VerificationRepository } from "../repositories/verification.repository";
 
 const userRepo = new UserRepository();
 const refreshSessionService = new RefreshSessionService();
-export const authService = new AuthService(userRepo, refreshSessionService);
+const verificationRepository = new VerificationRepository();
+export const authService = new AuthService(
+    userRepo,
+    refreshSessionService,
+    verificationRepository,
+);
 export const authController = new AuthController(authService);
